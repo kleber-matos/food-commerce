147fff7a4344ad500f504706e8516fe028629799
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2025-02-10 19:44:24 -0300

    :construction: Em progresso

---
+++ src/Components/Card_burguer/index.tsx
@@ -0,0 +1,9 @@
+import "./style.scss";
+
+export default function index() {
+  return (
+    <>
+      <h2>teste</h2>
+    </>
+  );
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,6 +3,11 @@ import Background from "../../Components/Background";
 import Logo from "../../assets/logo.png";
 import { CiStopwatch } from "react-icons/ci";
 import { FaStar } from "react-icons/fa";
+import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
+
+import data from "../../data/data.json";
+
+import Card_burguer from "../../Components/Card_burguer";
 
 import Barra from "../../Components/Barra";
 
@@ -35,8 +40,32 @@ export default function index() {
       <section>
         <Barra />
       </section>
-
       <Link to="/"> voltar</Link>
+
+      <Card_burguer />
+
+      {data.map((e) => (
+        <div className="burgue">
+          <img src={e.imagem} alt="teste" />
+          <h2>{e.nome}</h2>
+          <p>{e.descricao}</p>
+          <strong>{e.preco}</strong>
+          <p>{e.avaliacao}</p>
+          <p
+            style={e.disponivel == true ? { color: "green" } : { color: "red" }}
+          >
+            {e.disponivel == true ? (
+              <strong>
+                Disponível <AiFillCheckCircle />
+              </strong>
+            ) : (
+              <strong>
+                Indisponível <AiFillCloseCircle />
+              </strong>
+            )}
+          </p>
+        </div>
+      ))}
     </>
   );
 }
